7b2a7f782ae80b3dae52d8c934e4a7b0ff0e1ae2
fidelisaboke <fidelisaboke@gmail.com>
2024-06-13 11:16:01 +0300

    Update: Change Home page font-family to 'Montserrat'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
   </head>
-<body>
+<body class="font-montserrat">
     <header>
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
             <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
-                <a href="https://flowbite.com" class="flex items-center">
+                <a href="{{url('/')}}" class="flex items-center">
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">StrathPort</span>
                 </a>
                 @if(Route::has('login'))
