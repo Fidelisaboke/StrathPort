eb26ff0aa855c5d0ed2c179e000113698e4d05e0
Fidelisaboke <fidelisaboke@gmail.com>
2026-04-14 21:54:23 +0300

    chore: update entrypoint - Add command to seed roles + permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ set -e
 echo "[ENTRYPOINT] Running migrations..."
 php artisan migrate --force
 
+# Run RolePermissionSeeder
+echo "[ENTRYPOINT] Running RolePermissionSeeder..."
+php artisan db:seed --class=RolePermissionSeeder --force
+
 # Execute the main container command (FrankenPHP)
 echo "[ENTRYPOINT] Starting FrankenPHP..."
 exec frankenphp run --config /etc/caddy/Caddyfile
